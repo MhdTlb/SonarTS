160cb7b4b542db015574d05efcf23b053ec9e9fb
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-11-07 15:36:36 +0100

    reformat lint

--- sonarts-core/tests/rules/noUnreachableBlockRule/noUnreachableBlockRule.lint.ts
+++ sonarts-core/tests/rules/noUnreachableBlockRule/noUnreachableBlockRule.lint.ts
@@ -2,43 +2,43 @@ function alwaysTrueIf(x: number[], y: {}) {
   if (x) return 1;
   //  ^ {{This condition always evaluates to "true".}}
   if (!!y) return 1;
-  //  ^^^ {{This condition always evaluates to "true".}}  
+  //  ^^^ {{This condition always evaluates to "true".}}
 }
 
 function alwaysFalseIf(x: {}, y: undefined, z: null, w: void, u: undefined | null) {
   if (!x) return 1;
-//    ^^ {{This condition always evaluates to "false".}}
+  //  ^^ {{This condition always evaluates to "false".}}
   if (y) return 1;
-//    ^ {{This condition always evaluates to "false".}}
+  //  ^ {{This condition always evaluates to "false".}}
   if (z) return 1;
-//    ^ {{This condition always evaluates to "false".}}
+  //  ^ {{This condition always evaluates to "false".}}
   if (w) return 1;
-//    ^ {{This condition always evaluates to "false".}}
+  //  ^ {{This condition always evaluates to "false".}}
   if (u) return 1;
-//    ^ {{This condition always evaluates to "false".}}
+  //  ^ {{This condition always evaluates to "false".}}
   if ("") return 1;
-//    ^^ {{This condition always evaluates to "false".}}
+  //  ^^ {{This condition always evaluates to "false".}}
   if (0) return 1;
-//    ^ {{This condition always evaluates to "false".}}
+  //  ^ {{This condition always evaluates to "false".}}
   if (NaN) return 1; // FN
 }
 
 function alwaysFalseIfByConstraint(x: string) {
   if (x === "") {
     if (x) {
-//      ^ {{This condition always evaluates to "false".}}
+      //^ {{This condition always evaluates to "false".}}
     }
   }
 }
 
 function alwaysTrueTernary(x: number[]) {
   if (x) return 1;
-//    ^ {{This condition always evaluates to "true".}}
+  //  ^ {{This condition always evaluates to "true".}}
 }
 
 function l(x: number[]) {
   return x ? 1 : 2;
-//       ^ {{This condition always evaluates to "true".}}
+  //     ^ {{This condition always evaluates to "true".}}
 }
 
 function b(x: number) {
